2cae9e68ff2f27b35e1ddfebca146a995887c0de
ashishverma114142 <ashishverma114142@gmail.com>
2024-03-20 23:18:04 +0530

    Updates

---
+++ src/main/java/com/ServiceRegistory/Service/Registory/ServiceRegistoryApplication.java
@@ -0,0 +1,17 @@
+package com.ServiceRegistory.Service.Registory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistoryApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ServiceRegistoryApplication.class, args);
+
+	}
+
+}
